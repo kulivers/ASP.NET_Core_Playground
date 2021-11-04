a419eb55b5bb4fcf1dcd9a47a597051ab98f50b2
kulivers <kulivers@mail.ru>
2021-11-04 15:47:36 +0300

    trying to connect to db

--- ASP.NET Core Playground/Startup.cs
+++ ASP.NET Core Playground/Startup.cs
@@ -1,38 +1,69 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Parky_API.Data;
 
 namespace ASP.NET_Core_Playground
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        IWebHostEnvironment _env;
+        private IConfiguration _config;
+
+        public Startup(IWebHostEnvironment env, IConfiguration config)
+        {
+            _env = env;
+            _config = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            try
+            {
+                services.AddDbContext<ApplicationDataBaseContext>(options => options.UseSqlServer());
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseMiddleware<TokenMiddleware>();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Map("/index", Index);
+            app.UseEndpoints((endpoints) =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!ddd"); });
+                endpoints.MapGet("/index",
+                    async context => { await context.Response.WriteAsync("index endpoint"); });
             });
+
+            app.Run(async (context) => { await context.Response.WriteAsync("response"); });
+        }
+
+        private void Index(IApplicationBuilder app)
+        {
+            app.Run(async context => { await context.Response.WriteAsync("index method"); });
+        }
+
+        private static void HandleId(IApplicationBuilder app)
+        {
+            app.Run(async context => { await context.Response.WriteAsync("id is equal to 5"); });
         }
     }
 }
---
+++ ASP.NET Core Playground/TokenMiddleWare.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ASP.NET_Core_Playground
+{
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            
+            var token = context.Request.Query["token"];
+            if (token == "111")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync(token);
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
